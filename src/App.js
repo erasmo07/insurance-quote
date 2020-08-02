bf0a4850f6c8d696be4d1b6f47eb7c2e5a4aa2fc
erasmo <erasmomontes07@gmail.com>
2020-08-02 07:43:00 -0400

    Add component Header.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-;
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Cotizador de seguros</h1> 
+    <Header title='Cotizador de seguros'/>
   );
 }
 
